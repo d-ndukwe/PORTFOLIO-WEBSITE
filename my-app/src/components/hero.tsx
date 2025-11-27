6833fe5704d252e78a9f04dc9e6fb2d6d9da172b
ndukwedivinebball@gmail.com <ndukwedivinebball@gmail.com>
2025-11-27 23:11:32 +0100

    First Commit, Barely done anything

---
+++ my-app/src/components/hero.tsx
@@ -0,0 +1,20 @@
+
+
+function Hero() {
+    return(
+    <>
+        <section>
+            <div>
+                <h5>Hello! I'm</h5>
+                <h2>Divine <span>Ndukwe</span></h2>
+                <p></p>
+            </div>
+            <div></div>
+        </section>
+    </>
+    )
+
+    
+}
+
+export default Hero;
